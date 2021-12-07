cf632a0fdff4fdcc0a0b108957e12229887266ac
Richard Olivey <richard@olivey.net>
2021-12-07 13:22:20 +0000

    Update github URL

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -13,8 +13,8 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'oliveyrc', // Usually your GitHub org/user name.
+  projectName: 'oyster-docs', // Usually your repo name.
 
   presets: [
     [
